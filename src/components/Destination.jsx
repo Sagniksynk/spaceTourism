4131d2b5d26b9591aa2462ae3c03c5e028f6c90c
Chet25 <chetan25december@gmail.com>
2024-04-11 00:29:53 +0530

    testing spline

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,8 +1,5 @@
 import React, { useState, useRef, Suspense } from "react";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
-import { useLoader } from "@react-three/fiber";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Helmet } from "react-helmet";
 import Navbar from "./Navbar";
 import DestinationNav from "./DestinationNav";
 import data from "../assets/data.json";
@@ -10,44 +7,6 @@ import moon from "../assets/images/destination/image-moon.png";
 import mars from "../assets/images/destination/image-mars.png";
 import europa from "../assets/images/destination/image-europa.png";
 import titan from "../assets/images/destination/image-titan.png";
-
-//mars planet
-const Mars = () => {
-	const gltf = useLoader(GLTFLoader, "./mars.glb");
-	const marsRef = useRef();
-	useFrame(() => {
-		if (marsRef.current) {
-			marsRef.current.rotation.y += 0.004;
-		}
-	});
-	const modelProps = {
-		position: [0, -4, 0], // x, y, z
-		rotation: [0, 0, 0], // x, y, z (in radians)
-		scale: [3, 3, 3], // x, y, z
-	};
-
-	return <primitive object={gltf.scene} ref={marsRef} {...modelProps} />;
-};
-//europa planet
-const Europa = () => {
-	const gltf = useLoader(GLTFLoader, "./europa.glb");
-	const europaRef = useRef();
-	useFrame(() => {
-		if (europaRef.current) {
-			europaRef.current.rotation.y += 0.004;
-		}
-	});
-	const modelProps = {
-		position: [-2, -2, -2], // x, y, z
-		rotation: [0, 0, 0], // x, y, z (in radians)
-		scale: [2, 2, 2], // x, y, z
-	};
-
-	return (
-	<primitive ref={europaRef} object={gltf.scene} {...modelProps} />
-	);
-};
-
 
 
 const Destination = () => {
@@ -67,16 +26,23 @@ const Destination = () => {
 						src={pics[activeDestination]}
 						alt=''
 					/> */}
-					<div className="h-[170px] sm:h-56 md:h-[300px] aspect-square mx-auto mt-8 mb-5 sm:my-14 flex items-center justify-center">
-						<Canvas className="h-full w-full outline">
-							<ambientLight />
-							<pointLight position={[10, 10, 10]} />
-							<Suspense fallback={null}>
-								<Europa />
-							</Suspense>
-							<OrbitControls enableZoom={false} />
-						</Canvas>
-					</div>
+
+					<Helmet>
+					<script
+						type="module"
+						src="https://unpkg.com/@splinetool/viewer@1.0.93/build/spline-viewer.js"
+						></script>
+					</Helmet>
+					<spline-viewer
+						loading-anim-type="none"
+						url="https://prod.spline.design/CJ4YWQN7V3rHnGsp/scene.splinecode"
+						style={{ width: '100%', height: '300px' }} 
+						className="md:w-[500px] mx-auto" 
+						show-logo="false"
+					></spline-viewer>
+					
+
+
 					<div className=" md:flex md:flex-col md:text-left md:max-w-[450px] md:pr-11">
 						<DestinationNav
 							destination={activeDestination}
